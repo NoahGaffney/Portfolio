4509227483fc20454a800d95a5bdb37069784d00
NoahDGaff <113124344+NoahDGaff@users.noreply.github.com>
2024-10-10 21:44:51 -0400

    About me update
    Added to about me, stylized all pages, added more formatting to css

--- src/About.js
+++ src/About.js
@@ -1,10 +1,15 @@
 import Navbar from './Navbar';
+import Linkbar from './Linkbar'; 
 
 const About = () => {
     return (  
         <div className="About">
             <Navbar/>
-            <h2> About </h2>
+            <h2> -----------------About----------------- </h2>
+            <img src="me.JPG" className="picOfMe" />
+
+
+            <Linkbar/>
         </div>
 
     );
