c44fa5017245a48b4fda63ca8727089bc7b0067e
NoahDGaff <113124344+NoahDGaff@users.noreply.github.com>
2024-10-17 17:58:29 -0400

    leaps and bounds
    idk, i think it looks a lot better

--- src/About.js
+++ src/About.js
@@ -1,15 +1,15 @@
-
 const About = () => {
     return (  
         <div className="About">
             <h2> About </h2>
-            <img src="me.JPG" className="picOfMe" />
-
-
-            
+            <div className="about-content">
+                <img src="me.JPG" className="picOfMe" alt="Picture of me" />
+                <p className="about-text">
+                    Hello! I'm [Your Name]. I am a passionate developer with a love for coding and building amazing projects. I enjoy learning new technologies and constantly pushing my boundaries.
+                </p>
+            </div>
         </div>
-
     );
 }
- 
+
 export default About;
