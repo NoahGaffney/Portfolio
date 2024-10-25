23bbf6a7bf45dd3879e7c331556882e4e6930ebf
NoahDGaff <113124344+NoahDGaff@users.noreply.github.com>
2024-10-25 03:54:41 -0400

    resize hide navbar 3 line pop up
    title says it :)

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,34 @@
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (  
-        <nav className = "navbar">
+    return (
+        <nav className="navbar">
             <h1> Noah Gaffney</h1>
-            <div clasName="menu">
+            <div className="menu">
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
-            <div classname = "links">
-            <Link to="/">Home</Link>
-                    <Link to="/about">About Me</Link>
-                    <Link to="/education">Education</Link>
-                    <Link to="/experience">Experience</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/skills">Skills</Link>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/education">Education</Link>
+                <Link to="/experience">Experience</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/skills">Skills</Link>
+                <a href="/Resume2024_Noah_Gaffney.pdf" target="_blank" rel="noopener noreferrer">CV</a>
+            </div>
+            <div className="menu-items">
+                <Link to="/">Home</Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/education">Education</Link>
+                <Link to="/experience">Experience</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/skills">Skills</Link>
                 <a href="/Resume2024_Noah_Gaffney.pdf" target="_blank" rel="noopener noreferrer">CV</a>
             </div>
         </nav>
     );
 }
- 
+
 export default Navbar;
