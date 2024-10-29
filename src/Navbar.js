0376adac117e0287f73ce9ebd1276a823a33eff5
NoahDGaff <113124344+NoahDGaff@users.noreply.github.com>
2024-10-29 16:57:25 -0400

    Major Updates
    disregarding some minor bugs all that is left are education experience projects and skills

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,21 @@
 import { Link } from 'react-router-dom';
+import React, { useState } from "react"; 
 
 const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false)
+
     return (
         <nav className="navbar">
             <h1> Noah Gaffney</h1>
-            <div className="menu">
+            <div className="menu" onClick = {() => {
+                setMenuOpen(!menuOpen)
+            }}>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
-            <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/about">About Me</Link>
-                <Link to="/education">Education</Link>
-                <Link to="/experience">Experience</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/skills">Skills</Link>
-                <a href="/Resume2024_Noah_Gaffney.pdf" target="_blank" rel="noopener noreferrer">CV</a>
-            </div>
-            <div className="menu-items">
+            <div className={`links ${menuOpen ? "open" : ""}`}>
                 <Link to="/">Home</Link>
                 <Link to="/about">About Me</Link>
                 <Link to="/education">Education</Link>
