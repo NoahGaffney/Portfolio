5335386bf59e46dee320724517b6b8ac147330cb
NoahGaffney <noahdgaffney@gmail.com>
2026-04-13 08:31:14 -0400

    Update GitHub link in Linkbar component
    link was pointed to an old github profile that has no work done on it, pointed git here

--- src/Linkbar.js
+++ src/Linkbar.js
@@ -2,7 +2,7 @@ const Linkbar = () => {
     return (  
         <nav className="linkbar">
             <div className="socialLinks">
-                <a href="https://github.com/NoahDGaff" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/NoahGaffney" target="_blank" rel="noopener noreferrer">
                     <img src="github.png" alt="Github" className="png" />
                 </a>
                 <a href="https://www.linkedin.com/in/noah-micheal-gaffney/" target="_blank" rel="noopener noreferrer">
